93ef3afd605f093340ac2cd8d0be5bd6870ba1e8
alexis david <alexis.david@a4media.com>
2022-11-02 08:01:14 -0400

    make revisions in file in register_callback function

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -1,11 +1,11 @@
 from typing import Dict
 
 
-_callbacks: dict()
+_callbacks = {}
 
 def register(hook, order=0):
     def register_callback(func):
-      _callbacks.setdefault(hook, {}).setdefault(order, {}).append(func)
+      _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
       return func
     return register_callback
 
